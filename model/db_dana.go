8bee525cfa9bb71f93dd2f0e23b0d0232aaed7dc
bayuiqballl <bayuiqballl13@gmail.com>
2020-05-16 06:59:01 +0700

    add crud db method

--- model/db_dana.go
+++ model/db_dana.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"database/sql"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -32,9 +33,130 @@ func (store *DanaMysql) All() []Dana {
 
 	dana := Dana{}
 	for rows.Next() {
-		// rows.Scan(&siswa.ID, &siswa.Nama, &siswa.Nisn, &siswa.Pendidikan)
+		rows.Scan(
+			&dana.ID,
+			&dana.Judul,
+			&dana.Kategori,
+			&dana.Nama,
+			&dana.Organisasi,
+			&dana.Email,
+			&dana.Nominal,
+			&dana.Deskripsi,
+			&dana.Waktu_start,
+			&dana.Waktu_end,
+			&dana.Url,
+			&dana.Status,
+		)
 		danas = append(danas, dana)
 	}
 
 	return danas
+}
+
+func (store *DanaMysql) Save(dana *Dana) error {
+	result, err := store.DB.Exec(`INSERT INTO dana(judul,kategori,nama,organisasi,email,nominal,deskripsi,waktu_start,waktu_end,url,status) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)`, dana.Judul, dana.Kategori, dana.Nama, dana.Organisasi, dana.Email, dana.Nominal, dana.Deskripsi, dana.Waktu_start, dana.Waktu_end, dana.Url, dana.Status)
+	if err != nil {
+		return err
+	}
+	_, err = result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	// new id
+	lastID, err := result.LastInsertId()
+
+	dana.ID = int(lastID)
+
+	return nil
+}
+
+func (store *DanaMysql) Find(id int) *Dana {
+	dana := Dana{}
+
+	err := store.DB.QueryRow(`SELECT * FROM dana WHERE id=?`, id).Scan(
+		&dana.ID,
+		&dana.Judul,
+		&dana.Kategori,
+		&dana.Nama,
+		&dana.Organisasi,
+		&dana.Email,
+		&dana.Nominal,
+		&dana.Deskripsi,
+		&dana.Waktu_start,
+		&dana.Waktu_end,
+		&dana.Url,
+		&dana.Status,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return &dana
+}
+
+func (store *DanaMysql) Found(kategori int) *Dana {
+	dana := Dana{}
+
+	err := store.DB.QueryRow(`SELECT * FROM dana WHERE kategori=?`, kategori).Scan(
+		&dana.ID,
+		&dana.Judul,
+		&dana.Kategori,
+		&dana.Nama,
+		&dana.Organisasi,
+		&dana.Email,
+		&dana.Nominal,
+		&dana.Deskripsi,
+		&dana.Waktu_start,
+		&dana.Waktu_end,
+		&dana.Url,
+		&dana.Status,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return &dana
+}
+
+func (store *DanaMysql) Update(dana *Dana) error {
+	result, err := store.DB.Exec(`
+    UPDATE dana SET judul= ?, kategori= ?, nama = ?, organisasi = ?,email = ?, nominal = ?, deskripsi = ?, waktu_start = ?,waktu_end = ? WHERE id = ?`,
+		dana.Judul,
+		dana.Kategori,
+		dana.Nama,
+		dana.Organisasi,
+		dana.Email,
+		dana.Nominal,
+		dana.Deskripsi,
+		dana.Waktu_start,
+		dana.Waktu_end,
+		dana.ID,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (store *DanaMysql) Delete(dana *Dana) error {
+	result, err := store.DB.Exec(`DELETE FROM dana WHERE id = ?`, dana.ID)
+	if err != nil {
+		return err
+	}
+
+	_, err = result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	return nil
 }
